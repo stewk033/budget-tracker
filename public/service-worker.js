1bab5a4bb0f4b8c1367b2da076e6e29bc0651f42
stewk033 <kevin.c.stewart04@gmail.com>
2022-02-05 11:26:24 -0500

    refactor to accomodate files to cache

---
+++ public/service-worker.js
@@ -0,0 +1,16 @@
+const FILES_TO_CACHE = ["./index.html", "./css/styles.css", "./js/index.js"];
+
+const APP_PREFIX = "BudgetTrack-";
+const VERSION = "version_01";
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log(`installing cache: ${CACHE_NAME}`);
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("fetch", () => console.log("fetch"));
